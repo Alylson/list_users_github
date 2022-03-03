cb943541e39102d5c25a1bae65bc60cfb27ba139
vf <alylson@hotmail.com>
2022-03-03 10:18:37 -0300

    Listando usuário fixo no código

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UserDetail/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserDetail from './components/UserDetail.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserDetail
   }
 }
 </script>
---
+++ src/components/UserDetail.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="ui card" :key="user.id">
+      <div class="image">
+        <img :src="user.avatar_url" width="120" height="120">
+      </div>
+      <div class="content">
+        <div class="meta">
+          <label>{{ user.name }}</label>
+        </div>
+        <div class="description">
+            <a :href="user.html_url" target="_blank">Acessar perfil</a>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    name: "UserDetail",
+    data() {
+        return {
+            user: '',
+        };
+    },
+    created: function () {
+        axios.get("https://api.github.com/users/alylson").then((response) => {
+            this.user = response.data;
+        });
+    },
+};
+</script>
